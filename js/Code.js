7334ce0fa136989c9e54d86148f6eb91c3cf11bf
somyashah <76036213+somyashah@users.noreply.github.com>
2021-05-27 12:50:30 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "DATABASE";
+const accessCode3 = "FUNCTION";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("black")
+    text("Hint: Always changing, not constant !!", 100,70)
+ 
+    fill("white")
+    textSize(15)
+    text("ATADASBA",700,147)
+    fill("black")
+    text("Hint: Stores all the information !!",700,167)
+
+    fill("white")
+    textSize(15)
+    text("CUTNIFON",100,300)
+    fill("black")
+    text("Hint: Performs a specific task !!",100,330)
+
+}
